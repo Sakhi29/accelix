b5894486d32282aee33d694a30af88af819d01b5
Sakhi Rotliwala <rotliwalasakhi@gmail.com>
2024-12-17 17:31:14 +0530

    fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,11 @@ import type { Metadata } from "next";
 import { Montserrat, Kdam_Thmor_Pro } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
-import Loader from './components/Loader'
+import Loader from "./components/Loader";
 
 const montserrat = Montserrat({
-  subsets: ['latin'],
-  display: 'swap',
-});
-
-const kdamThmorPro = Kdam_Thmor_Pro({
-  subsets: ['latin'],
-  weight: '400',
-  display: 'swap',
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -23,7 +17,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
