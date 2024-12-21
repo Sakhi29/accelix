f6450caf03afb9e7ead027144d817ac26aa0f32a
ratish jain <ratishjain@ratishs-MacBook-Air.local>
2024-12-21 17:42:23 +0530

    components folder moved outside app directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar";
-import Loader from "./components/Loader";
+import Navbar from "../components/Navbar";
+import Loader from "../components/Loader";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
-import Hero from "./components/Hero";
-import Features from "./components/Features";
-import Services from "./components/Services";
-import CaseStudies from "./components/CaseStudies";
-import Testimonials from "./components/Testimonials";
-import Footer from "./components/Footer";
+import Hero from "../components/Hero";
+import Features from "../components/Features";
+import Services from "../components/Services";
+import CaseStudies from "../components/CaseStudies";
+import Testimonials from "../components/Testimonials";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <div className="">
       <main className="">
-        <Hero/>
-        <Features/>
-        <Services/>
-        <CaseStudies/>
-        <Testimonials/>
-        <Footer/>
+        <Hero />
+        <Features />
+        <Services />
+        <CaseStudies />
+        <Testimonials />
+        <Footer />
       </main>
     </div>
   );
